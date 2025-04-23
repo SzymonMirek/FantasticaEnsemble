bb1a9a1f0757c94cca53d7f1226f23afefb24dde
SzymonMirek <szymonmirek1@icloud.com>
2025-04-23 13:47:05 +0200

    Decap cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
     locales: ["pl", "en"],
     defaultLocale: "pl",
   },
+
   experimental: {
       svg: true,
     },
+
+  adapter: cloudflare(),
 });
